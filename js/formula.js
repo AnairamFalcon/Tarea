22f6f08b46fdb1b15413921f3e28c6e62cbcee4a
Anairam Falcón <ggf73659@udelasalle.edu.mx>
2021-08-24 23:33:59 -0500

    Formula general

---
+++ js/formula.js
@@ -0,0 +1,11 @@
+document.write("<h2>Equipo My Little Pony</h2>")
+
+var a=1;
+var b=-5;
+var c=-84;
+
+var formulap=((-b)+Math.pow(((Math.pow(b,2))-(4*a*c)),(1/2)))/(2*a);
+var formulan=((-b)-Math.pow(((Math.pow(b,2))-(4*a*c)),(1/2)))/(2*a);
+console.log("Las raíces de la ecuación con los valores de: a=",a+", b=",b+", c=",c, "es:")
+console.log("x1=",formulap)
+console.log("x2=",formulan)
